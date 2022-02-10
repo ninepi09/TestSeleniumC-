253f949ed72537087a4442753d6f41132a5916cc
ninepi09 <rizujikeda85@gmail.com>
2022-02-10 20:53:36 +0700

    initial commit

---
+++ seleniumCKresJavascript/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using NUnit.Framework;
+
+namespace seleniumCKresJavascript
+{
+    class Program
+    {
+
+        public static IWebDriver driver;
+
+        [Test]
+        public static void Main()
+        {
+
+            System.Environment.SetEnvironmentVariable("webdriver.chrome.driver", @"G:\\install\\selenium driver chrome\\chromedriver.exe");
+            driver =  new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/javascript_alerts");
+           
+
+            By jsAlertButton  = By.XPath(".//button[text()='Click for JS Alert']");
+
+            driver.FindElement(jsAlertButton).Click();
+            Console.WriteLine("Button Click for JS Alert Clicked");
+            Thread.Sleep(1000);
+
+            IAlert simpleAlert = driver.SwitchTo().Alert();
+            Thread.Sleep(1000);
+            simpleAlert.Accept();
+            Console.WriteLine("ok button on alert Clicked");
+        }
+
+        [Test]
+        public static void ClickJsConfirm()
+        {
+
+            driver =  new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/javascript_alerts");
+           
+
+            By jsConfirmButton  = By.XPath(".//button[text()='Click for JS Confirm']");
+
+            driver.FindElement(jsConfirmButton).Click();
+            Console.WriteLine("Button Click for JS Confirm Clicked");
+
+            Thread.Sleep(1000);
+
+            IAlert okAlert = driver.SwitchTo().Alert();
+            Thread.Sleep(1000);
+            okAlert.Accept();
+            Console.WriteLine("ok button on Confirm Clicked");
+
+            // code to click cancel
+            // IAlert cancelAlert = driver.SwitchTo().Alert();
+            // cancelAlert.Dismiss();
+            // Console.WriteLine("cancel button on Confirm Clicked");
+        }
+
+        [Test]
+        public static void ClickJsPrompt()
+        {
+
+             driver =  new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/javascript_alerts");
+           
+
+            By jsPromptButton  = By.XPath(".//button[text()='Click for JS Prompt']");
+
+            driver.FindElement(jsPromptButton).Click();
+            Console.WriteLine("Button Click for JS Prompt Clicked");
+
+            Thread.Sleep(1000);
+
+            IAlert okAlert = driver.SwitchTo().Alert();
+            Thread.Sleep(1000);
+            okAlert.SendKeys("Accepting the Prompt");
+            Thread.Sleep(1000);
+            okAlert.Accept();
+            Console.WriteLine("ok button on alert Clicked");
+
+
+            // code to click cancel
+            // IAlert cancelAlert = driver.SwitchTo().Alert();
+            // cancelAlert.Dismiss();
+            // Console.WriteLine("cancel button on Prompt Clicked");
+        }
+        
+    }
+}
---
+++ seleniumCompDbase/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using NUnit.Framework;
+
+namespace newSelenium
+{
+    class Program
+    {
+        public static IWebDriver driver;
+
+        [Test]
+        public static void Main()
+        {
+            System.Environment.SetEnvironmentVariable("webdriver.chrome.driver", @"G:\\install\\selenium driver chrome\\chromedriver.exe");
+            driver =  new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://computer-database.gatling.io/computers");
+           
+
+            By compdbaseSearch  = By.Name("f");
+            By computerDbaseSearchButt = By.Id("searchsubmit");
+            driver.FindElement(compdbaseSearch).SendKeys("ACE");
+            driver.FindElement(computerDbaseSearchButt).Click();
+
+            Console.WriteLine("Search box input ACE typed");
+
+            //open new browser
+            addNewComputer ();
+
+            
+            
+            // driver.Close();
+        }
+
+        [Test]
+        public static void addNewComputer (){
+
+            driver =  new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://computer-database.gatling.io/computers");
+
+            By addNewComButton = By.Id("add");
+
+            driver.FindElement(addNewComButton).Click();
+
+            Console.WriteLine("Add new computer button clicked");
+        }
+
+        [Test]
+        public static void addCreateNewComputer (){
+
+            driver =  new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://computer-database.gatling.io/computers/new");
+
+            By addNewComButton = By.ClassName("btn");
+            By cancelButton = By.LinkText("Cancel");
+            By computerName = By.Name("name");
+            By introduced = By.Id("introduced");
+            By discontinued = By.Id("discontinued");
+            By company = By.Name("company");
+
+            driver.FindElement(computerName).SendKeys("New Computer");
+            driver.FindElement(introduced).SendKeys("2022-02-22");
+            driver.FindElement(discontinued).SendKeys("2023-03-21");
+            driver.FindElement(company).SendKeys("Apple Inc");
+            driver.FindElement(addNewComButton).Click();
+
+            Console.WriteLine("Add new computer to dashboard");
+
+            //done
+            
+        }
+
+        [Test]
+        public static void cancelAddNewComp (){
+
+            driver =  new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://computer-database.gatling.io/computers/new");
+
+            By cancelButton = By.LinkText("Cancel");
+
+            driver.FindElement(cancelButton).Click();
+
+            Console.WriteLine("cancel button on add new computer clicked");
+
+            //done
+            
+        }
+    }
+}
